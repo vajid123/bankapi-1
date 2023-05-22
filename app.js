47d4735bfe2c396c6934cce60f162b1d3666b700
vajid123 <@gmail.com>
2023-05-22 16:44:41 +0530

    del

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Welcome...");
-});
-
 app.listen(7170, () => {
   console.log("Service running...");
 });
