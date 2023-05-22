98bb0ec010e304521143a0d3bd7912de78fa9888
vajid123 <@gmail.com>
2023-05-22 16:46:03 +0530

    hfh

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Welcome...");
+});
+
 app.listen(7170, () => {
   console.log("Service running...");
 });
+hello;
